3dc35394dcacb002f54ab07b84e9859cb9d3705e
John Heix Gmail <johnheix@gmail.com>
2026-03-27 15:02:23 -0400

    add url editor

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import { useEffect, useState } from "react";
 
+const URL = "https://gray-stone-0c7dad60f.2.azurestaticapps.net:5000/todos";
+
 function App() {
   const [todos, setTodos] = useState([]);
+  const [url, setUrl] = useState(URL);
+  const [reload, setReload] = useState(new Date());
 
   useEffect(() => {
-    fetch("https://gray-stone-0c7dad60f.2.azurestaticapps.net/todos")
+    fetch(url)
       .then((res) => res.json())
       .then((data) => setTodos(data));
-  }, []);
+  }, [reload]);
 
   // Local
   // useEffect(() => {
@@ -19,7 +23,20 @@ function App() {
   return (
     <div>
       <h1>Azure Todo App</h1>
-
+      <p>
+        Service URL: <span>{url}</span>
+      </p>
+      <P>
+        <input
+          type="text"
+          id="service-url"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+        />
+      </P>
+      <p>
+        <button onClick={setReload(new Date())}>Reload</button>
+      </p>
       {todos.map((todo) => (
         <p key={todo.id}>{todo.title}</p>
       ))}
